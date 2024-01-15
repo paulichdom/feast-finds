9eca724a3a3c6d69cecf47e89d1cb53776352a2e
Domagoj <paulic.domagoj@gmail.com>
2024-01-15 16:54:53 +0100

    explore routing (4)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const testTube = { target: 'client', message: 'Hi there' };
+  //res.send('Hey! It works!');
+  //res.json(testTube);
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
